00a098d53ce6fe7615f0e4538fd7b3c8804b9db7
Gary Kane <garykane@csus.edu>
2023-10-02 16:58:04 -0700

    Adding a new python file to teach students python is fun and easy

---
+++ intro.py
@@ -0,0 +1,33 @@
+# Python
+
+# Simplicity and Readability
+def greet(name):
+    print(f"Hello, {name}!")
+
+name = "Alice"
+greet(name)
+
+# Dynamic Typing
+x = 5
+x = "Hello"
+
+# Indentation Matters
+if True:
+    print("This is indented")
+
+# No Explicit Data Types
+num = 42
+name = "John"
+
+# Lists vs. Arrays
+my_list = [1, "two", 3.0]
+
+# Built-in High-Level Data Types
+my_dict = {"name": "Alice", "age": 30}
+my_set = {1, 2, 3}
+
+# No Explicit Memory Management
+# (No equivalent example in Python for manual memory management)
+
+# Interpreted Language
+print("Hello, World!")
